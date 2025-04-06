dc993a8e9aa40f809789527f5ac1e3e110691c46
Navam9530 <mushamnavam9530@gmail.com>
2025-04-06 18:40:28 +0530

    first commit

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1743944804',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/discoveries/',
+      
+        '/inventions/',
+      
+        '/archives/',
+      
+        '/about-me/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
